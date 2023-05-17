cd5d71ccbf6ba627630e2273f354ed289512a1fe
carlos renan <carlosrenan96@gmail.com>
2023-05-16 22:20:56 -0300

    feat: base components

---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,11 @@
+export default function Join() {
+    return (
+        <div>
+          <h1>
+            Join
+          </h1>
+          <input type="text" placeholder="Nome de usuário" />
+          <button>Entrar</button>
+        </div>
+    )
+}
