f07b5944f304baf0be17d9d2c84fe5caf0bc5ef3
carlos renan <carlosrenan96@gmail.com>
2023-05-19 21:00:21 -0300

    feat: basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 
 import Join from './components/Join/Join'
 import Chat from './components/Chat/Chat'
+import { useState } from 'react'
 
 function App() {
+  const [chatVisibily, setChatVisibily] = useState(false)
 
   return (
     <div className='App'>
-      <Join />
-      <Chat />
+      { chatVisibily ? <Chat /> : <Join setChatVisibily={setChatVisibily} />}
     </div>
   )
 }
--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,11 +1,18 @@
-export default function Join() {
-    return (
-        <div>
-          <h1>
-            Join
-          </h1>
-          <input type="text" placeholder="Nome de usuário" />
-          <button>Entrar</button>
-        </div>
-    )
+import { useRef } from "react"
+
+export default function Join({setChatVisibily}) {
+  const usernameRef = useRef()
+
+  const handleSubmit = () => {
+    const username = usernameRef.current.value
+    if(!username.trim()) return
+    setChatVisibily(true)
+  }
+  return (
+      <div>
+        <h1>Join</h1>
+        <input type="text" ref={usernameRef} placeholder="Nome de usuário" />
+        <button onClick={() => handleSubmit()}>Entrar</button>
+      </div>
+  )
 }
