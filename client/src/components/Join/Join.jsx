4cee0267dde05a38e9153e7047a3d9cb1a6ee9d3
carlos renan <carlosrenan96@gmail.com>
2023-05-19 21:03:34 -0300

    feat: basic socket connection

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,12 +1,16 @@
 import { useRef } from "react"
+import io from 'socket.io-client'
 
 export default function Join({setChatVisibily}) {
   const usernameRef = useRef()
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const username = usernameRef.current.value
     if(!username.trim()) return
+    
+    const socket = await io.connect('http://localhost:3001')
     setChatVisibily(true)
+
   }
   return (
       <div>
